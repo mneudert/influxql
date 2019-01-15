749e34b43999522d855851b07e935f1a548e191e
Marc Neudert <marc.neudert@gmail.com>
2019-01-15 19:45:09 +0100

    configures dialyzer warnings

--- mix.exs
+++ mix.exs
@@ -11,6 +11,7 @@ defmodule InfluxQL.Mixfile do
       elixir: "~> 1.3",
       deps: deps(),
       description: "InfluxQL utility/tooling package",
+      dialyzer: dialyzer(),
       docs: docs(),
       package: package(),
       preferred_cli_env: [
@@ -28,6 +29,17 @@ defmodule InfluxQL.Mixfile do
       {:dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test, runtime: false}
+    ]
+  end
+
+  defp dialyzer do
+    [
+      flags: [
+        :error_handling,
+        :race_conditions,
+        :underspecs,
+        :unmatched_returns
+      ]
     ]
   end
 
