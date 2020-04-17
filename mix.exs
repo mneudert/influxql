5d6bf4f82276cb4589adf31de3045228d434f23d
Marc Neudert <marc.neudert@gmail.com>
2020-04-17 17:31:32 +0200

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule InfluxQL.Mixfile do
   defp deps do
     [
       {:credo, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.12", only: :test, runtime: false}
     ]
