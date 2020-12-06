b08b8264c8bfec795202ec3c8222f6393d45f404
Marc Neudert <marc.neudert@gmail.com>
2020-12-06 13:15:33 +0100

    Properly configure GitHub actions dialyzer plt cache

--- mix.exs
+++ mix.exs
@@ -39,7 +39,9 @@ defmodule InfluxQL.Mixfile do
         :race_conditions,
         :underspecs,
         :unmatched_returns
-      ]
+      ],
+      plt_core_path: "plts",
+      plt_file: {:no_warn, "plts/dialyzer.plt"}
     ]
   end
 
